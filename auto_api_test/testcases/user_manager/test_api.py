26c60d32689cdd48262677c5c7fb71c67b2324f0
zhangyiyuan_air <1421800536@qq.com>
2022-06-12 14:27:53 +0800

    用例整理

--- auto_api_test/testcases/user_manager/test_api.py
+++ auto_api_test/testcases/user_manager/test_api.py
@@ -11,16 +11,16 @@ from commons.yaml_unit import write_yaml, clear_yaml, read_yaml, read_yaml_key, 
 from hotload.debug_talk import debug_talk
 
 
-class Test_api:
-
-    @pytest.mark.parametrize("caseinfo",parametrize_ddt("/testcases/user_manager/test_token.yaml"))
-    def test_get_token(self,caseinfo):
-        RequestUtil(debug_talk()).standard_yaml_testcase(caseinfo)
-
-
-    @pytest.mark.parametrize("caseinfo",parametrize_ddt("/testcases/user_manager/edit_flag.yaml"))
-    def test_edit_flag(self,caseinfo):
-        RequestUtil(debug_talk()).standard_yaml_testcase(caseinfo)
+# class Test_api:
+#
+#     @pytest.mark.parametrize("caseinfo",parametrize_ddt("/testcases/user_manager/test_token.yaml"))
+#     def test_get_token(self,caseinfo):
+#         RequestUtil(debug_talk()).standard_yaml_testcase(caseinfo)
+#
+#
+#     @pytest.mark.parametrize("caseinfo",parametrize_ddt("/testcases/user_manager/edit_flag.yaml"))
+#     def test_edit_flag(self,caseinfo):
+#         RequestUtil(debug_talk()).standard_yaml_testcase(caseinfo)
     #
     # @pytest.mark.parametrize("caseinfo", parametrize_ddt("/testcases/user_manager/select_flag.yaml"))
     # def test_select_flag(self,caseinfo):
